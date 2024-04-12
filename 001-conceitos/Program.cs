c36fadcd31ff3c635ac35803d17142652a591630
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-12 15:45:43 -0300

    Orientação a Objetos com C#  - Classes, atributos, métodos e objetos

---
+++ 001-conceitos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace conceitos001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciar objeto
+             Pessoa obj = new Pessoa();
+             obj.nome = "Ralf";
+             obj.idade = 30;
+             obj.mensagem();
+        }
+    }
+}
