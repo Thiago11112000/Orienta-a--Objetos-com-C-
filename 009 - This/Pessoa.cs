5fa94f715e1db36d5f24e7a3f7331ecb8e7aaf36
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-16 23:07:07 -0300

    This

---
+++ 009 - This/Pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    public class Pessoa
+    {
+        private string nome = "Tatiana";
+
+
+        public Pessoa(string nome)
+         {
+            Console.WriteLine(nome);
+           Console.WriteLine(this.nome);
+         }
+
+    }
---
+++ 009 - This/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _009___This
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("Thiago");
+        }
+    }
+}
