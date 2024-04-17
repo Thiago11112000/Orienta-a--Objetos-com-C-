c24ef92635af5a8857a962e36195e29b2f6fc2be
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-17 10:44:16 -0300

    Interface

---
+++ 14- Interface/IPadrao.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    public interface IPadrao
+    {
+        void somar (int n1, int n2);
+
+        void subtrair(int n1, int n2);
+    }
