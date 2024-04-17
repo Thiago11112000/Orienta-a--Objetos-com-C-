021af0e6dbcb0331aa6524286c8469b544b196ec
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-16 22:06:44 -0300

    Modificadores de acesso

---
+++ 005 - Acesso/Produto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//  public > atributos e métodos visíveis em qualquer classe
+// private > atributos e métodos visíveis apenas na classe onde são criadas
+ // protected > atributos e métodos visíveis em classes  onde são criadas ou herdados
+
+    public class Produto
+    {
+        public string nome;
+        private double valor;
+    }
