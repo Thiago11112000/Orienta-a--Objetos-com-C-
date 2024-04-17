5ed55f6aeea22687b936c0ae260a1050abab45dd
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-16 22:17:15 -0300

    Encapsulamento

---
+++ 006 - Encapsulamento/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _006___Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+            a.mensagem();
+        }
+    }
+}
