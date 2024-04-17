792fd9f43c2dadbf13cab3f2a18076602c18a95f
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-16 22:30:33 -0300

    Get e set

---
+++ 008 - Get e Set/Pessoa.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    public class Pessoa
+    {
+        // Get e Set
+
+        private string nome;
+        public string Nome 
+        {
+            get{return nome;}
+            set{nome = value;}
+        }
+        
+
+
+    }
---
+++ 008 - Get e Set/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _008___Get_e_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = "Thiago";
+            Console.WriteLine(p.Nome);
+        }
+    }
+}
