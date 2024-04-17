2b6fdf03be7d258eabbffa5c91f2a210e4723aa4
Thiago11112000 <thiagoclick2000@hotmail.com>
2024-04-17 09:58:07 -0300

    Método static

---
+++ 12 - Estático/Exemplo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+    public class Exemplo
+    {
+        public static string nome = "Thiago";
+        public static void  soma(int n1 , int n2)
+        {
+        Console.WriteLine(n1 + n2);
+        }
+    }
---
+++ 12 - Estático/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _12___Estático
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Exemplo.soma(3, 7);
+           Console.WriteLine("Olá " + Exemplo.nome );
+        }
+    }
+}
